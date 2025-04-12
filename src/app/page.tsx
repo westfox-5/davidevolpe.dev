12cdba0983ccbbd02c5a5c8bb1e38898920f0520
Davide Volpe <volpe.davide.vd@gmail.com>
2025-04-12 19:44:53 +0200

    Update styles and animations in globals.css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 "use client"
 
 import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home() {
+const Home = () => {
+
+  const [cvPosition, setCvPosition] = useState({ x: 0, y: 0 });
+  const cvRef = useRef<HTMLDivElement | null>(null); // Riferimento al div di destinazione
+
+
+  useEffect(() => {
+    const getCvPosition = () => {
+      if (cvRef.current) {
+        const rect = cvRef.current.getBoundingClientRect();
+        setCvPosition({
+          x: rect.left + rect.width / 2, // X centrata
+          y: rect.top + rect.height / 2,  // Y centrata
+        });
+      }
+    };
+
+    // Aggiorna la posizione al caricamento iniziale
+    getCvPosition();
+
+    // Aggiungi un event listener per ridimensionamento della finestra
+    window.addEventListener('resize', getCvPosition);
+
+    return () => {
+      window.removeEventListener('resize', getCvPosition);
+    };
+  }, []);
+
   return (
     <div>
       <nav className="sticky top-0 backdrop-blur
@@ -13,14 +41,14 @@ export default function Home() {
         after:h-1
         after:w-full
         after:content-[' ']">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+        <div className="max-w-screen-xl flex items-center justify-between p-4">
           <a href="https://davidevolpe.dev" className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-3xl lg:text-4xl font-semibold whitespace-nowrap dark:text-white">
               Davide Volpe
             </span>
           </a>
 
-          <div className="w-full md:block md:w-auto text-xl lg:text-xl font-medium">
+          <div className="md:block md:w-auto text-xl lg:text-xl font-medium">
             <ul className="flex flex-col p-4 md:p-0 rounded-lg md:flex-row rtl:space-x-reverse">
               <li>
                 <a href="/files/cv_en.pdf" target="_blank" rel="noopener noreferrer" className="block py-2 md:p-0">
@@ -42,22 +70,24 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="flex items-center justify-evenly mx-auto p-4">
-        <motion.span className="blur-bubble" animate={['initial']} variants={{
-          initial: {
-            y: [0, -200],
-            x: [0, 500],
-            rotate: [0, -30, -45],
-            scale: [1, 1.5, 2, 1.5, 1, 0.5],
-            transition: {
-              duration: 20,
-              repeat: Infinity,
-              repeatType: 'reverse'
-            }
-          }
-        }}></motion.span>
+      <div className="flex items-center justify-center">
+        <motion.div className="blur-bubble"
+          animate={{
+            x: ["-100%", "45%", "%"],
+            y: ["-20%", "50%", "0%"],
+            scaleX: [2, 1.5, 1], // Cambia la scala dell'elemento
+          }}
+          transition={{
+            duration: 10,              // Durata totale di una transizione
+            //repeat: Infnity,         // Ripete l'animazione indefinitamente
+            //repeatType: "loop",       // Tipo di ripetizione
+            ease: "easeInOut",        // Tipo di transizione
+            delay: 2,                 // Aggiungi un ritardo singolo per l'animazione
+            times: [2, 3, 2, 0],  // Ogni animazione ha un comportamento più casuale
+          }}
+        ></motion.div>
 
-        <div className="flex flex-col gap-8 pl-40 py-10">
+        <div className="flex flex-col gap-8 pl-10 py-10">
           <div className="font-extrabold text-5xl lg:text-8xl">
             Hello, I&apos;m <br />
             <span className="text-[var(--background)] text-outline">
@@ -72,7 +102,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex text-center items-center justify-center text-4xl lg:text-5xl mt-10">
+      <div className="flex flex-wrap w-full text-center items-center justify-center text-xl lg:text-5xl mt-10 p-8">
         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline fill-current w-9">
           <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path>
         </svg>
@@ -85,6 +115,8 @@ export default function Home() {
       </div>
 
       <div className="h-96"></div>
-    </div>
+    </div >
   );
 }
+
+export default Home;
